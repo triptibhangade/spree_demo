ff89b92a369faa2cfec1c486380482cd9e75c3b9
User4 <tripti.bhangade@gemsessence.com>
2019-09-09 15:13:18 +0530

    add contact us page in main nav bar

--- app/overrides/add_profile_email.rb
+++ app/overrides/add_profile_email.rb
@@ -2,6 +2,6 @@ Deface::Override.new(virtual_path:'spree/shared/_main_nav_bar',
   name: 'add profile name to main nav bar',
   insert_after: '#home-link',
   text: ' <li class="profile">
-            <%= current_spree_user.try(:email) %>
+            <a href="/contact_us"><%= current_spree_user.try(:email) %></a>
           </li> '
 )
--- config/routes.rb
+++ config/routes.rb
@@ -9,4 +9,8 @@ Rails.application.routes.draw do
   # the default of "spree".
   mount Spree::Core::Engine, at: '/'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  Spree::Core::Engine.routes.draw do
+    # get :contact_us, to: "home#contact_us"
+     get '/contact_us', to: 'home#contact_us'
+  end
 end
