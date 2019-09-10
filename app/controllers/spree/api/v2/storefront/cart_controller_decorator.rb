9c2e2bfa15ca688fa3fcb394f3436932aaaf905f
User4 <tripti.bhangade@gemsessence.com>
2019-09-10 13:21:44 +0530

    Cart_controller.......

--- app/controllers/spree/api/v2/storefront/cart_controller_decorator.rb
+++ app/controllers/spree/api/v2/storefront/cart_controller_decorator.rb
@@ -1,5 +1,6 @@
 Spree::Api::V2::Storefront::CartController.class_eval do
-  def add_item
+  before_action :cart_count, only: [:add_item]
+  def add_item   
     variant = Spree::Variant.find(params[:variant_id])
 
     spree_authorize! :update, spree_current_order, order_token
@@ -13,5 +14,14 @@ Spree::Api::V2::Storefront::CartController.class_eval do
     )
 
     render_order(result)
+  end
+
+  def cart_count
+    current_spree_user.orders.each do |order|
+        binding.pry
+      if order.id == spree_current_order.id
+        order.line_items.first.cart_count = order.line_items.first.cart_count.to_i + 1  if order.line_items 
+      end
+    end
   end
 end
