05dab6fee62dc19fa52d2c0cad08d1b0833ed288
User4 <tripti.bhangade@gemsessence.com>
2019-09-11 12:09:08 +0530

    send temporary mail by contact us

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,7 +1,8 @@
 class ContactMailer < ApplicationMailer
   default from: 'spree@gmail.com'
  
-  def thank_you_email
+  def thank_you_email(contact)
+    @contact = contact
     @user = params[:user]
     @url  = 'http://localhost:3000/login'
     mail(to: @user.email, subject: 'Thank you for giving response')
